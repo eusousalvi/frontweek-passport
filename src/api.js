606b5e37f3f8442a236828fe2d1a44b8767436ea
Gabriel <gabrielss@outlook.com>
2021-06-22 00:48:52 -0300

    [passport]: finish passport card

--- src/api.js
+++ src/api.js
@@ -1,4 +1,7 @@
 const formElement = document.querySelector("#form");
+const passportName = document.querySelector("#name");
+const passportLogin = document.querySelector("#login");
+const passportImg = document.querySelector("#img");
 
 async function getDataFromGithub(username) {
   return await fetch(`https://api.github.com/users/${username}`).then((response) => response.json());
@@ -17,6 +20,13 @@ formElement.addEventListener("submit", async (e) => {
   e.preventDefault();
   const userData = await getDataFromGithub(e.target.login.value);
   const { login, name, avatar_url } = userData;
+  passportLogin.innerText = login;
+  passportName.innerText = name;
+  passportImg.src = avatar_url;
+  passportImg.style.opacity = 1;
+  passportImg.style.width = '100%';
+  passportImg.style.height = '100%';
   const user = { login, name, avatar_url };
+
   downloadImage(formElement, `frontend-week-passport-${name}`);
 });
