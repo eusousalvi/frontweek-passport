55b0f0d8d59ba4361fefd221911786024ab9920c
Gabriel <gabrielss@outlook.com>
2021-06-21 22:32:37 -0300

    [api]: integrate api

--- src/api.js
+++ src/api.js
@@ -1,13 +1,13 @@
-const formElement = document.querySelector("#passport-form");
-const inputElement = document.querySelector('input[type="text"]');
-
-let userData = {};
+const formElement = document.querySelector("#form");
 
 async function getDataFromGithub(username) {
   return await fetch(`https://api.github.com/users/${username}`).then((response) => response.json());
 }
 
-formElement.addEventListener("submit", async (event) => {
-  event.preventDefault();
-  userData = await getDataFromGithub(inputElement.value);
+formElement.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const userData = await getDataFromGithub(e.target.login.value);
+  const {login, name, avatar_url} = userData;
+  const user = {login, name, avatar_url};
+  console.log(user)
 });
